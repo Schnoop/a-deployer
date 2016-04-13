60ee1a97651358b134036e98ad792830cb952e67
Thorsten Daners <thorsten.daners@antwerpes.de>
2016-04-13 12:46:27 +0200

    Test include of files too.

--- tests/unit/Service/IncludesTest.php
+++ tests/unit/Service/IncludesTest.php
@@ -5,8 +5,21 @@
  */
 class IncludesTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * 
+     */
     public function testFilter()
     {
+        $files = [
+            'tests/demo-folder/demo2/fooBar.txt',
+            'tests/demo-folder/fooBar.txt',
+            'tests/demo-folder/demo1/demo1.1/demo1.1.1/fooBar.txt',
+            'tests/demo-folder/demo1/barFoo.txt',
+        ];
+        foreach ($files as $file) {
+            @unlink($file);
+        }
+
         $folders = [
             'tests/demo-folder/demo2/demo2.1',
             'tests/demo-folder/demo2',
@@ -27,14 +40,21 @@ class IncludesTest extends PHPUnit_Framework_TestCase
         foreach ($folders as $folder) {
             @mkdir($folder, 0777, true);
         }
+        foreach ($files as $file) {
+            @file_put_contents($file, 'demo');
+        }
 
         $foldersProof = [
             'tests/demo-folder/demo2/demo2.1',
+            'tests/demo-folder/demo2/fooBar.txt',
             'tests/demo-folder/demo2',
             'tests/demo-folder/demo3',
             'tests/demo-folder/demo4',
+            'tests/demo-folder/fooBar.txt',
+            'tests/demo-folder/demo1/demo1.1/demo1.1.1/fooBar.txt',
             'tests/demo-folder/demo1/demo1.1/demo1.1.1',
             'tests/demo-folder/demo1/demo1.1',
+            'tests/demo-folder/demo1/barFoo.txt',
             'tests/demo-folder/demo1',
         ];
 
