9ed4ff804f457a21dc9006d74b82c0a890ef2017
Thorsten Daners <thorsten.daners@antwerpes.de>
2016-04-13 17:10:36 +0200

    Removed useless code.

--- src/Visuals/DryRun.php
+++ src/Visuals/DryRun.php
@@ -44,56 +44,10 @@ class DryRun
         }
 
         $tableCells[] = [new TableCell('<info>Files that will be uploaded.</info>')];
-        $tableCells[] = [new TableCell('File')];
 
         $table = new Table($output);
         $table->setHeaders($tableCells)->setRows($tableRows);
         $table->render();
         $output->writeln('');
-    }
-
-    /**
-     * Convert $bytes in human readable size.
-     *
-     * @param int $bytes
-     *
-     * @return string
-     */
-    private function fileSizeConvert($bytes)
-    {
-        $result = 0;
-        $bytes = floatval($bytes);
-        $arBytes = [
-            0 => [
-                'UNIT'  => 'TB',
-                'VALUE' => pow(1024, 4),
-            ],
-            1 => [
-                'UNIT'  => 'GB',
-                'VALUE' => pow(1024, 3),
-            ],
-            2 => [
-                'UNIT'  => 'MB',
-                'VALUE' => pow(1024, 2),
-            ],
-            3 => [
-                'UNIT'  => 'KB',
-                'VALUE' => 1024,
-            ],
-            4 => [
-                'UNIT'  => 'B',
-                'VALUE' => 1,
-            ],
-        ];
-
-        foreach ($arBytes as $arItem) {
-            if ($bytes >= $arItem['VALUE']) {
-                $result = $bytes / $arItem['VALUE'];
-                $result = str_replace('.', ',', strval(round($result, 2))).' '.$arItem['UNIT'];
-                break;
-            }
-        }
-
-        return $result;
     }
 }
