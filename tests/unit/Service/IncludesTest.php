b697061c61f78d7e8a51f8451e1dd3cd6ab11de8
Schnoop <thorsten@daners.info>
2016-04-14 08:35:16 -0400

    Applied fixes from StyleCI

--- tests/unit/Service/IncludesTest.php
+++ tests/unit/Service/IncludesTest.php
@@ -7,39 +7,39 @@ class IncludesTest extends PHPUnit_Framework_TestCase
 {
     public function testFilter()
     {
-        $baseFolder = 'demo-folder-' . date('his', time());
+        $baseFolder = 'demo-folder-'.date('his', time());
         $folders = [
-            'tests/' . $baseFolder .'/demo2/demo2.1',
-            'tests/' . $baseFolder .'/demo2',
-            'tests/' . $baseFolder .'/demo3',
-            'tests/' . $baseFolder .'/demo4',
-            'tests/' . $baseFolder .'/demo1/demo1.1/demo1.1.1',
-            'tests/' . $baseFolder .'/demo1/demo1.1',
-            'tests/' . $baseFolder .'/demo1',
+            'tests/'.$baseFolder.'/demo2/demo2.1',
+            'tests/'.$baseFolder.'/demo2',
+            'tests/'.$baseFolder.'/demo3',
+            'tests/'.$baseFolder.'/demo4',
+            'tests/'.$baseFolder.'/demo1/demo1.1/demo1.1.1',
+            'tests/'.$baseFolder.'/demo1/demo1.1',
+            'tests/'.$baseFolder.'/demo1',
         ];
 
         foreach ($folders as $folder) {
             @rmdir($folder);
         }
-        @rmdir('tests/' . $baseFolder .'/');
+        @rmdir('tests/'.$baseFolder.'/');
 
         foreach ($folders as $folder) {
             @mkdir($folder, 0777, true);
         }
 
         $foldersProof = [
-            'tests/' . $baseFolder .'/demo2/demo2.1',
-            'tests/' . $baseFolder .'/demo2',
-            'tests/' . $baseFolder .'/demo3',
-            'tests/' . $baseFolder .'/demo4',
-            'tests/' . $baseFolder .'/demo1/demo1.1/demo1.1.1',
-            'tests/' . $baseFolder .'/demo1/demo1.1',
-            'tests/' . $baseFolder .'/demo1',
+            'tests/'.$baseFolder.'/demo2/demo2.1',
+            'tests/'.$baseFolder.'/demo2',
+            'tests/'.$baseFolder.'/demo3',
+            'tests/'.$baseFolder.'/demo4',
+            'tests/'.$baseFolder.'/demo1/demo1.1/demo1.1.1',
+            'tests/'.$baseFolder.'/demo1/demo1.1',
+            'tests/'.$baseFolder.'/demo1',
         ];
 
         $transfer = new \Antwerpes\ADeployer\Model\Transfer();
         $includes = new \Antwerpes\ADeployer\Service\Includes();
-        $transfer = $includes->add($transfer, ['tests/' . $baseFolder]);
+        $transfer = $includes->add($transfer, ['tests/'.$baseFolder]);
         $this->assertEquals($foldersProof, $transfer->getFilesToUpload());
     }
 }
